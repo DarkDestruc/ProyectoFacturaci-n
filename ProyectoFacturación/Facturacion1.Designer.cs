80505d7eefb58ea54d5909f954edcbb88acaa8c9
Moises Chavez <gchavez19956@gmail.com>
2024-03-20 23:43:36 -0500

    Add project files.

---
+++ ProyectoFacturación/Facturacion1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace ProyectoFacturación
+{
+    partial class Facturacion1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbcabecera = new System.Windows.Forms.GroupBox();
+            this.gpbDetalleFact = new System.Windows.Forms.GroupBox();
+            this.lblcliente = new System.Windows.Forms.Label();
+            this.lblidentificacion = new System.Windows.Forms.Label();
+            this.lbldireccion = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblNoFact = new System.Windows.Forms.Label();
+            this.lblSecFact = new System.Windows.Forms.Label();
+            this.lblFFact = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSb12 = new System.Windows.Forms.Label();
+            this.lblSb0 = new System.Windows.Forms.Label();
+            this.lvliva = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblSbTotal = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.gpbcabecera.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbcabecera
+            // 
+            this.gpbcabecera.Controls.Add(this.textBox8);
+            this.gpbcabecera.Controls.Add(this.textBox7);
+            this.gpbcabecera.Controls.Add(this.textBox6);
+            this.gpbcabecera.Controls.Add(this.textBox5);
+            this.gpbcabecera.Controls.Add(this.textBox4);
+            this.gpbcabecera.Controls.Add(this.lblSbTotal);
+            this.gpbcabecera.Controls.Add(this.lblTotal);
+            this.gpbcabecera.Controls.Add(this.lvliva);
+            this.gpbcabecera.Controls.Add(this.lblSb0);
+            this.gpbcabecera.Controls.Add(this.lblSb12);
+            this.gpbcabecera.Controls.Add(this.label1);
+            this.gpbcabecera.Controls.Add(this.lblFFact);
+            this.gpbcabecera.Controls.Add(this.lblSecFact);
+            this.gpbcabecera.Controls.Add(this.lblNoFact);
+            this.gpbcabecera.Controls.Add(this.textBox3);
+            this.gpbcabecera.Controls.Add(this.textBox2);
+            this.gpbcabecera.Controls.Add(this.textBox1);
+            this.gpbcabecera.Controls.Add(this.lbldireccion);
+            this.gpbcabecera.Controls.Add(this.lblidentificacion);
+            this.gpbcabecera.Controls.Add(this.lblcliente);
+            this.gpbcabecera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gpbcabecera.Location = new System.Drawing.Point(0, 0);
+            this.gpbcabecera.Name = "gpbcabecera";
+            this.gpbcabecera.Size = new System.Drawing.Size(800, 238);
+            this.gpbcabecera.TabIndex = 0;
+            this.gpbcabecera.TabStop = false;
+            this.gpbcabecera.Text = "Cabecera de Facturacion";
+            // 
+            // gpbDetalleFact
+            // 
+            this.gpbDetalleFact.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gpbDetalleFact.Location = new System.Drawing.Point(0, 238);
+            this.gpbDetalleFact.Name = "gpbDetalleFact";
+            this.gpbDetalleFact.Size = new System.Drawing.Size(800, 212);
+            this.gpbDetalleFact.TabIndex = 1;
+            this.gpbDetalleFact.TabStop = false;
+            this.gpbDetalleFact.Text = "Detalle_Facturacion";
+            // 
+            // lblcliente
+            // 
+            this.lblcliente.AutoSize = true;
+            this.lblcliente.Location = new System.Drawing.Point(29, 29);
+            this.lblcliente.Name = "lblcliente";
+            this.lblcliente.Size = new System.Drawing.Size(42, 13);
+            this.lblcliente.TabIndex = 0;
+            this.lblcliente.Text = "Cliente:";
+            // 
+            // lblidentificacion
+            // 
+            this.lblidentificacion.AutoSize = true;
+            this.lblidentificacion.Location = new System.Drawing.Point(29, 63);
+            this.lblidentificacion.Name = "lblidentificacion";
+            this.lblidentificacion.Size = new System.Drawing.Size(54, 13);
+            this.lblidentificacion.TabIndex = 1;
+            this.lblidentificacion.Text = "C.I./RUC:";
+            // 
+            // lbldireccion
+            // 
+            this.lbldireccion.AutoSize = true;
+            this.lbldireccion.Location = new System.Drawing.Point(29, 96);
+            this.lbldireccion.Name = "lbldireccion";
+            this.lbldireccion.Size = new System.Drawing.Size(55, 13);
+            this.lbldireccion.TabIndex = 2;
+            this.lbldireccion.Text = "Direccion:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(88, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(298, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // lblNoFact
+            // 
+            this.lblNoFact.AutoSize = true;
+            this.lblNoFact.Location = new System.Drawing.Point(473, 29);
+            this.lblNoFact.Name = "lblNoFact";
+            this.lblNoFact.Size = new System.Drawing.Size(89, 13);
+            this.lblNoFact.TabIndex = 6;
+            this.lblNoFact.Text = "Numero_Factura:";
+            // 
+            // lblSecFact
+            // 
+            this.lblSecFact.AutoSize = true;
+            this.lblSecFact.Location = new System.Drawing.Point(584, 29);
+            this.lblSecFact.Name = "lblSecFact";
+            this.lblSecFact.Size = new System.Drawing.Size(13, 13);
+            this.lblSecFact.TabIndex = 7;
+            this.lblSecFact.Text = "1";
+            // 
+            // lblFFact
+            // 
+            this.lblFFact.AutoSize = true;
+            this.lblFFact.Location = new System.Drawing.Point(473, 63);
+            this.lblFFact.Name = "lblFFact";
+            this.lblFFact.Size = new System.Drawing.Size(79, 13);
+            this.lblFFact.TabIndex = 8;
+            this.lblFFact.Text = "Fecha Factura:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(584, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "20/03/2024";
+            // 
+            // lblSb12
+            // 
+            this.lblSb12.AutoSize = true;
+            this.lblSb12.Location = new System.Drawing.Point(473, 96);
+            this.lblSb12.Name = "lblSb12";
+            this.lblSb12.Size = new System.Drawing.Size(93, 13);
+            this.lblSb12.TabIndex = 10;
+            this.lblSb12.Text = "SUBTOTAL 12% :";
+            // 
+            // lblSb0
+            // 
+            this.lblSb0.AutoSize = true;
+            this.lblSb0.Location = new System.Drawing.Point(473, 124);
+            this.lblSb0.Name = "lblSb0";
+            this.lblSb0.Size = new System.Drawing.Size(87, 13);
+            this.lblSb0.TabIndex = 11;
+            this.lblSb0.Text = "SUBTOTAL 0% :";
+            // 
+            // lvliva
+            // 
+            this.lvliva.AutoSize = true;
+            this.lvliva.Location = new System.Drawing.Point(473, 150);
+            this.lvliva.Name = "lvliva";
+            this.lvliva.Size = new System.Drawing.Size(30, 13);
+            this.lvliva.TabIndex = 12;
+            this.lvliva.Text = "IVA :";
+            this.lvliva.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(473, 202);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(48, 13);
+            this.lblTotal.TabIndex = 13;
+            this.lblTotal.Text = "TOTAL :";
+            // 
+            // lblSbTotal
+            // 
+            this.lblSbTotal.AutoSize = true;
+            this.lblSbTotal.Location = new System.Drawing.Point(473, 176);
+            this.lblSbTotal.Name = "lblSbTotal";
+            this.lblSbTotal.Size = new System.Drawing.Size(70, 13);
+            this.lblSbTotal.TabIndex = 14;
+            this.lblSbTotal.Text = "SUBTOTAL :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(587, 89);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(95, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(587, 117);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(95, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(587, 143);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(95, 20);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(587, 169);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(95, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(587, 195);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(95, 20);
+            this.textBox8.TabIndex = 19;
+            // 
+            // Facturacion1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gpbDetalleFact);
+            this.Controls.Add(this.gpbcabecera);
+            this.Name = "Facturacion1";
+            this.Text = "Facturacion1";
+            this.gpbcabecera.ResumeLayout(false);
+            this.gpbcabecera.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbcabecera;
+        private System.Windows.Forms.Label lbldireccion;
+        private System.Windows.Forms.Label lblidentificacion;
+        private System.Windows.Forms.Label lblcliente;
+        private System.Windows.Forms.GroupBox gpbDetalleFact;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFFact;
+        private System.Windows.Forms.Label lblSecFact;
+        private System.Windows.Forms.Label lblNoFact;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSb0;
+        private System.Windows.Forms.Label lblSb12;
+        private System.Windows.Forms.Label lvliva;
+        private System.Windows.Forms.Label lblSbTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
